a17069250f93f97c2e85863bd63f29bc3090fd48
max <mk@kernel-corp.com>
2014-05-12 14:43:43 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Websocket::Application.config.secret_key_base = '33038600390d79edff80ec82e1dc16154783a2a91b05854b0c34415b39c6fb7bd67e58c03d2d7a62e4f7957438f9fade2b140888331bfde17b56b05cf4fda77c'

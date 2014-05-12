507117040fcb7a4cfe7a2d1a30376cfcb0d6ef33
max <mk@kernel-corp.com>
2014-05-12 15:59:40 +0700

    thin config

---
+++ config/deploy.rb
@@ -0,0 +1,77 @@
+
+require 'rvm/capistrano' # Для работы rvm
+require 'bundler/capistrano' # Для работы bundler. При изменении гемов bundler автоматически обновит все гемы на сервере, чтобы они в точности соответствовали гемам разработчика.
+
+
+set :application, 'websocket'
+
+set :rails_env, 'production'
+#set :domain, 'mkonin@137.117.230.217'
+set :domain, 'mkonin@185.4.75.151'
+set :deploy_to, "/var/www/#{application}"
+set :use_sudo, false
+set :unicorn_conf, "#{deploy_to}/current/config/unicorn.rb"
+set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
+set :normalize_asset_timestamps, false
+set :keep_releases, 5
+
+set :rvm_ruby_string, 'ruby-2.0.0-p247'
+
+
+set :scm, :git
+set :repository, 'https://github.com/KernelCorp/websocket.git'
+set :branch, 'master' # Ветка из которой будем тянуть код для деплоя.
+set :deploy_via, :remote_cache # Указание на то, что стоит хранить кеш репозитария локально и с каждым деплоем лишь подтягивать произведенные изменения. Очень актуально для больших и тяжелых репозитариев.
+
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
+
+before 'deploy:setup', 'rvm:install_rvm', 'rvm:install_ruby'
+after 'deploy:update', 'deploy:cleanup'
+
+# Generate an additional task to fire up the thin clusters
+namespace :deploy do
+  desc "Start the Thin processes"
+  task :start do
+    run  <<-CMD
+      cd /var/www/apps/current; bundle exec thin start -C config/thin.yml
+    CMD
+  end
+
+  desc "Stop the Thin processes"
+  task :stop do
+    run <<-CMD
+      cd #{deploy_to}/current; bundle exec thin stop -C config/thin.yml
+    CMD
+  end
+
+  desc "Restart the Thin processes"
+  task :restart do
+    run <<-CMD
+      cd #{deploy_to}/current; bundle exec thin restart -C config/thin.yml
+    CMD
+  end
+
+end
+
+# Define all the tasks that need to be running manually after Capistrano is finished.
+after "deploy:finalize_update", "deploy:restart"
+after "deploy", "deploy:migrate"
+
+
+# For websocets
+namespace :websocket do
+  task :start do
+    run " cd #{deploy_to}/current && bundle exec rake RAILS_ENV=#{rails_env} websocket_rails:start_server"
+  end
+
+  task :stop do
+    run "cd #{deploy_to}/current && bundle exec rake RAILS_ENV=#{rails_env} websocket_rails:stop_server"
+  end
+
+  task :restart do
+    run " cd #{deploy_to}/current && bundle exec rake RAILS_ENV=#{rails_env} websocket_rails:stop_server"
+    run " cd #{deploy_to}/current && bundle exec rake RAILS_ENV=#{rails_env} websocket_rails:start_server"
+  end
+end
